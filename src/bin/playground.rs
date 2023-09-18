a90a4887e5aec3f86f56dc5dd30f18655df1e031
Rafal Pasko <rp.261000@gmail.com>
2023-09-18 21:05:05 +0200

    Cow

--- src/bin/playground.rs
+++ src/bin/playground.rs
@@ -1,32 +1,5 @@
 fn main() {
-    let data = vec![1, 2, 3];
+    let r1 = r#"He said "Goodbye" and left"#;
 
-    let closure = move || {
-        // The closure takes ownership of `data`
-        println!("Here's your data: {:?}", data);
-    };
-    println!("Here's your data: {:?}", data); // wont work
-
-    // Call the closure
-    closure();
-    closure();
-
-    let data2 = vec![1, 2, 3];
-
-    let closure2 =  || {
-        // The closure takes ownership of `data`
-        for d in data2 {
-            println!("Here's your data: {:?}", d);
-        }
-
-        for d in data2 {
-            println!("Here's your data: {:?}", d);
-        }
-    };
-
-    closure2();
-    closure2(); // cant call it twice
-
-
-
+    println!("{}", r1);
 }
